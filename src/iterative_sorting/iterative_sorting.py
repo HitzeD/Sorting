7586d1737ca7b23d22d3aa6c9e82eba42cf4f618
HitzeD <d-hitze@comcast.net>
2019-09-09 18:32:18 -0400

    bubble sort complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -26,40 +26,29 @@ def selection_sort( arr ):
     return arr
 
 a = [5, 2, 8, 7, 3]
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 
-def insertion_sort(list):
-
-# mark first element as sorted (starting the list at index 1 rather than 0)
-# for each unsorted element X
-    for i in range(1, len(list)):
-
-        # extract the element X
-        temp = list[i]
-
-        # j = lastSortedIndex down to 0
-        j = i
-        print(f"\nCur_Index: {i}\nTemp: {temp}\nJ: {j}\n")
-        # if current element j > x
-        while j > 0 and temp < list[j - 1]:
-            # move sorted element to the right by one
-            list[j] = list[j - 1]
-            # change j
-            j -= 1
-            break
-        
-        list[j] = temp
-        
-    return list
-
-selection_sort(a)
-# insertion_sort(a)
+# selection_sort(a)
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    for i in range(0, len(arr) - 1):
 
+        small = i
+        temp = arr[i]
+        j = i
+
+        for j in range(i, len(arr)):
+            if(arr[i] > arr[j]):
+                temp = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp
+    print(arr)
     return arr
 
+# bubble_sort(a)
+bubble_sort(arr1)
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
