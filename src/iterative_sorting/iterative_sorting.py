0845753095a4e35c28ec9263035e7bcd813b42f9
HitzeD <d-hitze@comcast.net>
2019-09-09 17:38:46 -0400

    selective sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,20 +2,57 @@
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
 
+        smallest_index = i
+        temp = arr[i]
+        j = i 
 
-
-        # TO-DO: swap
-
-
-
-
+        # for j in range(i, len(arr)):
+        #     if arr[smallest_index] > arr[j]:
+        #         smallest_index = arr[j]
+        
+        # temp = arr[i]
+        # arr[i] = arr[smallest_index]
+        # arr[smallest_index] = temp
+        
+        while j < len(arr):
+            if (arr[smallest_index] > arr[j]):
+                smallest_index = j
+            j += 1
+        temp = arr[i]
+        arr[i] = arr[smallest_index]
+        arr[smallest_index] = temp
+    print(arr)
     return arr
+
+a = [5, 2, 8, 7, 3]
+
+def insertion_sort(list):
+
+# mark first element as sorted (starting the list at index 1 rather than 0)
+# for each unsorted element X
+    for i in range(1, len(list)):
+
+        # extract the element X
+        temp = list[i]
+
+        # j = lastSortedIndex down to 0
+        j = i
+        print(f"\nCur_Index: {i}\nTemp: {temp}\nJ: {j}\n")
+        # if current element j > x
+        while j > 0 and temp < list[j - 1]:
+            # move sorted element to the right by one
+            list[j] = list[j - 1]
+            # change j
+            j -= 1
+            break
+        
+        list[j] = temp
+        
+    return list
+
+selection_sort(a)
+# insertion_sort(a)
 
 
 # TO-DO:  implement the Bubble Sort function below
